86a70e8fb71cdf0ade6ea6faabf896ddb44c98bf
Xueting Tao <xtao8@jhu.edu>
2017-09-01 10:49:17 -0400

    the package that need for data improting

---
+++ prepare.R
@@ -0,0 +1,9 @@
+#the tools to get data from the websit
+install.packages('rvest')
+install.packages('stringr')
+install.packages('tidyr')
+#library:
+library(rvest)
+library(stringr)
+library(tidyr)
+
